cfb9ff71e1f3b206e6708f88976b6c5afefc82d6
Manisha Dey <manishadey2020@gmail.com>
2021-10-01 15:01:02 +0530

    Added the routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ColorsComponent } from './colors/colors.component';
 import {HttpClientModule} from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AnothercolorsComponent } from './anothercolors/anothercolors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorsComponent
+    ColorsComponent,
+    PageNotFoundComponent,
+    AnothercolorsComponent
   ],
   imports: [
     BrowserModule,
